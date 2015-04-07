d8c72c368e0c592645fc515282ff880abbe88999
Python1320 <python1320@users.noreply.github.com>
2015-04-08 02:01:06 +0300

    GeoIP

--- install.cmd
+++ install.cmd
@@ -6,6 +6,17 @@ if NOT EXIST ../includes GOTO NOTOK2
 ROBOCOPY extra/garrysmod ../../ *.* /s
 ROBOCOPY extra\serverquery\lua ../ *.* /s
 ROBOCOPY extra\vstruct\lua ../ *.* /s
+
+if exist ..\..\..\GeoIP.dat goto skipgeoip
+echo Downloading GeoIP...
+powershell -command "(new-object System.Net.WebClient).DownloadFile('http://geolite.maxmind.com/download/geoip/database/GeoLiteCity.dat.gz', 'GeoIP.dat.gz')"
+echo Extracting GeoIP.dat.gz and copying to where hl2.exe is
+gunzip GeoIP.dat.gz
+copy GeoIP.dat ..\..\..\GeoIP.dat
+:skipgeoip
+
+
+
 goto EOF
 :NOTOK1
 @ echo Missing menu2\extra\vstruct\lua\vstruct\vstruct (did you git clone with submodules, recursively)
