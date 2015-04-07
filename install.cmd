51cb7e86d0fe0a29955c311d3384cffc6545cb07
Python1320 <python1320@users.noreply.github.com>
2015-04-08 01:26:50 +0300

    make installer smarter

--- install.cmd
+++ install.cmd
@@ -1,5 +1,17 @@
 @echo off
+if NOT EXIST extra\vstruct\lua\vstruct\vstruct GOTO NOTOK1
+if NOT EXIST ../../steam.inf GOTO NOTOK2
+if NOT EXIST ../includes GOTO NOTOK2
+
 ROBOCOPY extra/garrysmod ../../ *.* /s
 ROBOCOPY extra\serverquery\lua ../ *.* /s
 ROBOCOPY extra\vstruct\lua ../ *.* /s
+goto EOF
+:NOTOK1
+@ echo Missing menu2\extra\vstruct\lua\vstruct\vstruct (did you git clone with submodules, recursively)
+goto EOF
+:NOTOK2
+@ echo Installed in wrong folder
+goto EOF
+:EOF
 pause
