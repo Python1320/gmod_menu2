091d6bb142b99b02f4bd6cb3a0b239a9e30a3355
Python1320 <python1320@users.noreply.github.com>
2015-12-21 03:48:53 +0200

    Update installation and submodule reference

--- install.cmd
+++ install.cmd
@@ -1,21 +1,42 @@
 @echo off
+
 if exist extra goto skipclone
 
-git clone --recursive --branch luaserverlist https://github.com/Python1320/gmod_menu2
+if NOT EXIST ../steam.inf GOTO NOTOK3
+
+
+if exist menu2 goto skipclone_goto
+
+git clone --recursive --branch luaserverlist https://github.com/Python1320/gmod_menu2 menu2
 goto cloned
 
+:skipclone_goto
+cd menu2
+call install.cmd
+exit 0
+
 :skipclone
+git pull
 git submodule foreach git pull
+goto skipclone2
+
 :cloned
+
+if NOT EXIST menu2 GOTO NOTOK0
+cd menu2
+:skipclone2
+
 
 if NOT EXIST extra\vstruct\lua\vstruct\vstruct GOTO NOTOK1
 if NOT EXIST ../../steam.inf GOTO NOTOK2
 if NOT EXIST ../includes GOTO NOTOK2
 
-ROBOCOPY extra/garrysmod ../../ *.* /s
-ROBOCOPY extra\serverquery\lua ../ *.* /s
-ROBOCOPY extra\vstruct\lua ../ *.* /s
+echo Copying files...
+ROBOCOPY extra/garrysmod ../../ *.* /s /NFL /NDL /NJH /NJS
+ROBOCOPY extra\serverquery\lua ../ *.* /s /NFL /NDL /NJH /NJS
+ROBOCOPY extra\vstruct\lua ../ *.* /s /NFL /NDL /NJH /NJS
 
+echo GeoIP file check..
 if exist ..\..\..\GeoIP.dat goto skipgeoip
 echo Downloading GeoIP...
 powershell -command "(new-object System.Net.WebClient).DownloadFile('http://geolite.maxmind.com/download/geoip/database/GeoLiteCity.dat.gz', 'GeoIP.dat.gz')"
@@ -24,14 +45,22 @@ gunzip GeoIP.dat.gz
 copy GeoIP.dat ..\..\..\GeoIP.dat
 :skipgeoip
 
-
+echo === ALL DONE, maybe ===
 
 goto EOF
+
+:NOTOK0
+@ echo Missing menu2 (could not clone?)
+goto EOF
+
 :NOTOK1
 @ echo Missing menu2\extra\vstruct\lua\vstruct\vstruct (did you git clone with submodules, recursively)
 goto EOF
 :NOTOK2
-@ echo Installed in wrong folder
+@ echo Installed in wrong folder (Put me to garrysmod/lua/install.cmd)
+goto EOF
+:NOTOK3
+@ echo Installed in wrong folder (Put me to garrysmod/lua/install.cmd)
 goto EOF
 :EOF
 pause
